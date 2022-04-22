bff602265529f5fcdfc595f36e42015a52106943
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-04-22 22:22:16 +0700

    add package and dependencies

---
+++ lib/features/weather/presentation/bloc/weather_state.dart
@@ -0,0 +1,9 @@
+part of 'weather_bloc.dart';
+
+abstract class WeatherState extends Equatable {
+  const WeatherState();  
+
+  @override
+  List<Object> get props => [];
+}
+class WeatherInitial extends WeatherState {}
