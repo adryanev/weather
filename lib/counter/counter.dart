9173d5ca0ff2b9b37294d2bc15fda44f2a7d834a
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-04-22 22:11:55 +0700

    remove counter

--- lib/counter/counter.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022, Adryan Eka Vandra
-// https://github.com/adryanev
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-export 'cubit/counter_cubit.dart';
-export 'view/counter_page.dart';
--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -7,13 +7,11 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weather/app/app.dart';
-import 'package:weather/counter/counter.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
     });
   });
 }
