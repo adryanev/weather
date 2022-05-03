c407d313fde54c42ffd03e86b676db792b1806c3
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-05-03 09:39:18 +0700

    sort import

--- lib/features/splash/domain/usecases/fetch_remote_config_api_key_use_case.dart
+++ lib/features/splash/domain/usecases/fetch_remote_config_api_key_use_case.dart
@@ -1,5 +1,5 @@
-import 'package:weather/core/domain/failures/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:weather/core/domain/failures/failure.dart';
 import 'package:weather/core/domain/usecases/use_case.dart';
 import 'package:weather/features/splash/domain/repositories/remote_config_repository.dart';
 
