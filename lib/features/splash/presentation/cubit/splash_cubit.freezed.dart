325eb3e07b0b7c1e6321169d4bb3c8c89ffb30f2
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-06-06 21:45:53 +0700

    add splash cubit

---
+++ lib/features/splash/presentation/cubit/splash_cubit.freezed.dart
@@ -0,0 +1,282 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'splash_cubit.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$SplashStateTearOff {
+  const _$SplashStateTearOff();
+
+  _SplashState call(
+      {required Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiUrlOrFailureOption,
+      required Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiKeyOrFailureOption,
+      required Option<Either<Failure, Unit>> saveApiKeyOrFailureOption,
+      required Option<Either<Failure, Unit>> saveApiUrlOrFailureOption,
+      required SplashStatus status,
+      required bool isLoading}) {
+    return _SplashState(
+      fetchApiUrlOrFailureOption: fetchApiUrlOrFailureOption,
+      fetchApiKeyOrFailureOption: fetchApiKeyOrFailureOption,
+      saveApiKeyOrFailureOption: saveApiKeyOrFailureOption,
+      saveApiUrlOrFailureOption: saveApiUrlOrFailureOption,
+      status: status,
+      isLoading: isLoading,
+    );
+  }
+}
+
+/// @nodoc
+const $SplashState = _$SplashStateTearOff();
+
+/// @nodoc
+mixin _$SplashState {
+  Option<Either<Failure, RemoteConfig<String, String>>>
+      get fetchApiUrlOrFailureOption => throw _privateConstructorUsedError;
+  Option<Either<Failure, RemoteConfig<String, String>>>
+      get fetchApiKeyOrFailureOption => throw _privateConstructorUsedError;
+  Option<Either<Failure, Unit>> get saveApiKeyOrFailureOption =>
+      throw _privateConstructorUsedError;
+  Option<Either<Failure, Unit>> get saveApiUrlOrFailureOption =>
+      throw _privateConstructorUsedError;
+  SplashStatus get status => throw _privateConstructorUsedError;
+  bool get isLoading => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $SplashStateCopyWith<SplashState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SplashStateCopyWith<$Res> {
+  factory $SplashStateCopyWith(
+          SplashState value, $Res Function(SplashState) then) =
+      _$SplashStateCopyWithImpl<$Res>;
+  $Res call(
+      {Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiUrlOrFailureOption,
+      Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiKeyOrFailureOption,
+      Option<Either<Failure, Unit>> saveApiKeyOrFailureOption,
+      Option<Either<Failure, Unit>> saveApiUrlOrFailureOption,
+      SplashStatus status,
+      bool isLoading});
+}
+
+/// @nodoc
+class _$SplashStateCopyWithImpl<$Res> implements $SplashStateCopyWith<$Res> {
+  _$SplashStateCopyWithImpl(this._value, this._then);
+
+  final SplashState _value;
+  // ignore: unused_field
+  final $Res Function(SplashState) _then;
+
+  @override
+  $Res call({
+    Object? fetchApiUrlOrFailureOption = freezed,
+    Object? fetchApiKeyOrFailureOption = freezed,
+    Object? saveApiKeyOrFailureOption = freezed,
+    Object? saveApiUrlOrFailureOption = freezed,
+    Object? status = freezed,
+    Object? isLoading = freezed,
+  }) {
+    return _then(_value.copyWith(
+      fetchApiUrlOrFailureOption: fetchApiUrlOrFailureOption == freezed
+          ? _value.fetchApiUrlOrFailureOption
+          : fetchApiUrlOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, RemoteConfig<String, String>>>,
+      fetchApiKeyOrFailureOption: fetchApiKeyOrFailureOption == freezed
+          ? _value.fetchApiKeyOrFailureOption
+          : fetchApiKeyOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, RemoteConfig<String, String>>>,
+      saveApiKeyOrFailureOption: saveApiKeyOrFailureOption == freezed
+          ? _value.saveApiKeyOrFailureOption
+          : saveApiKeyOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, Unit>>,
+      saveApiUrlOrFailureOption: saveApiUrlOrFailureOption == freezed
+          ? _value.saveApiUrlOrFailureOption
+          : saveApiUrlOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, Unit>>,
+      status: status == freezed
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as SplashStatus,
+      isLoading: isLoading == freezed
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$SplashStateCopyWith<$Res>
+    implements $SplashStateCopyWith<$Res> {
+  factory _$SplashStateCopyWith(
+          _SplashState value, $Res Function(_SplashState) then) =
+      __$SplashStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiUrlOrFailureOption,
+      Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiKeyOrFailureOption,
+      Option<Either<Failure, Unit>> saveApiKeyOrFailureOption,
+      Option<Either<Failure, Unit>> saveApiUrlOrFailureOption,
+      SplashStatus status,
+      bool isLoading});
+}
+
+/// @nodoc
+class __$SplashStateCopyWithImpl<$Res> extends _$SplashStateCopyWithImpl<$Res>
+    implements _$SplashStateCopyWith<$Res> {
+  __$SplashStateCopyWithImpl(
+      _SplashState _value, $Res Function(_SplashState) _then)
+      : super(_value, (v) => _then(v as _SplashState));
+
+  @override
+  _SplashState get _value => super._value as _SplashState;
+
+  @override
+  $Res call({
+    Object? fetchApiUrlOrFailureOption = freezed,
+    Object? fetchApiKeyOrFailureOption = freezed,
+    Object? saveApiKeyOrFailureOption = freezed,
+    Object? saveApiUrlOrFailureOption = freezed,
+    Object? status = freezed,
+    Object? isLoading = freezed,
+  }) {
+    return _then(_SplashState(
+      fetchApiUrlOrFailureOption: fetchApiUrlOrFailureOption == freezed
+          ? _value.fetchApiUrlOrFailureOption
+          : fetchApiUrlOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, RemoteConfig<String, String>>>,
+      fetchApiKeyOrFailureOption: fetchApiKeyOrFailureOption == freezed
+          ? _value.fetchApiKeyOrFailureOption
+          : fetchApiKeyOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, RemoteConfig<String, String>>>,
+      saveApiKeyOrFailureOption: saveApiKeyOrFailureOption == freezed
+          ? _value.saveApiKeyOrFailureOption
+          : saveApiKeyOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, Unit>>,
+      saveApiUrlOrFailureOption: saveApiUrlOrFailureOption == freezed
+          ? _value.saveApiUrlOrFailureOption
+          : saveApiUrlOrFailureOption // ignore: cast_nullable_to_non_nullable
+              as Option<Either<Failure, Unit>>,
+      status: status == freezed
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as SplashStatus,
+      isLoading: isLoading == freezed
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_SplashState implements _SplashState {
+  const _$_SplashState(
+      {required this.fetchApiUrlOrFailureOption,
+      required this.fetchApiKeyOrFailureOption,
+      required this.saveApiKeyOrFailureOption,
+      required this.saveApiUrlOrFailureOption,
+      required this.status,
+      required this.isLoading});
+
+  @override
+  final Option<Either<Failure, RemoteConfig<String, String>>>
+      fetchApiUrlOrFailureOption;
+  @override
+  final Option<Either<Failure, RemoteConfig<String, String>>>
+      fetchApiKeyOrFailureOption;
+  @override
+  final Option<Either<Failure, Unit>> saveApiKeyOrFailureOption;
+  @override
+  final Option<Either<Failure, Unit>> saveApiUrlOrFailureOption;
+  @override
+  final SplashStatus status;
+  @override
+  final bool isLoading;
+
+  @override
+  String toString() {
+    return 'SplashState(fetchApiUrlOrFailureOption: $fetchApiUrlOrFailureOption, fetchApiKeyOrFailureOption: $fetchApiKeyOrFailureOption, saveApiKeyOrFailureOption: $saveApiKeyOrFailureOption, saveApiUrlOrFailureOption: $saveApiUrlOrFailureOption, status: $status, isLoading: $isLoading)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _SplashState &&
+            const DeepCollectionEquality().equals(
+                other.fetchApiUrlOrFailureOption, fetchApiUrlOrFailureOption) &&
+            const DeepCollectionEquality().equals(
+                other.fetchApiKeyOrFailureOption, fetchApiKeyOrFailureOption) &&
+            const DeepCollectionEquality().equals(
+                other.saveApiKeyOrFailureOption, saveApiKeyOrFailureOption) &&
+            const DeepCollectionEquality().equals(
+                other.saveApiUrlOrFailureOption, saveApiUrlOrFailureOption) &&
+            const DeepCollectionEquality().equals(other.status, status) &&
+            const DeepCollectionEquality().equals(other.isLoading, isLoading));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(fetchApiUrlOrFailureOption),
+      const DeepCollectionEquality().hash(fetchApiKeyOrFailureOption),
+      const DeepCollectionEquality().hash(saveApiKeyOrFailureOption),
+      const DeepCollectionEquality().hash(saveApiUrlOrFailureOption),
+      const DeepCollectionEquality().hash(status),
+      const DeepCollectionEquality().hash(isLoading));
+
+  @JsonKey(ignore: true)
+  @override
+  _$SplashStateCopyWith<_SplashState> get copyWith =>
+      __$SplashStateCopyWithImpl<_SplashState>(this, _$identity);
+}
+
+abstract class _SplashState implements SplashState {
+  const factory _SplashState(
+      {required Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiUrlOrFailureOption,
+      required Option<Either<Failure, RemoteConfig<String, String>>>
+          fetchApiKeyOrFailureOption,
+      required Option<Either<Failure, Unit>> saveApiKeyOrFailureOption,
+      required Option<Either<Failure, Unit>> saveApiUrlOrFailureOption,
+      required SplashStatus status,
+      required bool isLoading}) = _$_SplashState;
+
+  @override
+  Option<Either<Failure, RemoteConfig<String, String>>>
+      get fetchApiUrlOrFailureOption;
+  @override
+  Option<Either<Failure, RemoteConfig<String, String>>>
+      get fetchApiKeyOrFailureOption;
+  @override
+  Option<Either<Failure, Unit>> get saveApiKeyOrFailureOption;
+  @override
+  Option<Either<Failure, Unit>> get saveApiUrlOrFailureOption;
+  @override
+  SplashStatus get status;
+  @override
+  bool get isLoading;
+  @override
+  @JsonKey(ignore: true)
+  _$SplashStateCopyWith<_SplashState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
--- lib/injector.config.dart
+++ lib/injector.config.dart
@@ -5,24 +5,29 @@
 // **************************************************************************
 
 import 'package:firebase_remote_config/firebase_remote_config.dart' as _i3;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i5;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import 'core/data/storages/local_storage.dart' as _i4;
-import 'core/di/firebase_module.dart' as _i13;
+import 'core/data/storages/local_storage.dart' as _i5;
+import 'core/di/app_module.dart' as _i16;
+import 'core/di/firebase_module.dart' as _i15;
 import 'features/splash/data/datasources/local_config_data_source.dart' as _i7;
 import 'features/splash/data/datasources/remote_config_data_source.dart' as _i6;
 import 'features/splash/data/repositories/remote_config_repository_impl.dart'
     as _i9;
 import 'features/splash/domain/repositories/remote_config_repository.dart'
     as _i8;
-import 'features/splash/domain/usecases/fetch_remote_config_api_url_use_case.dart'
+import 'features/splash/domain/usecases/fetch_remote_config_api_key_use_case.dart'
     as _i12;
+import 'features/splash/domain/usecases/fetch_remote_config_api_url_use_case.dart'
+    as _i13;
 import 'features/splash/domain/usecases/save_remote_config_api_key.dart'
     as _i10;
 import 'features/splash/domain/usecases/save_remote_config_api_url.dart'
-    as _i11; // ignore_for_file: unnecessary_lambdas
+    as _i11;
+import 'features/splash/presentation/cubit/splash_cubit.dart'
+    as _i14; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -30,13 +35,15 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   final firebaseModule = _$FirebaseModule();
+  final appModule = _$AppModule();
   gh.lazySingleton<_i3.FirebaseRemoteConfig>(() => firebaseModule.remoteConfig);
-  gh.lazySingleton<_i4.LocalStorage>(
-      () => _i4.LocalStorageImpl(get<_i5.FlutterSecureStorage>()));
+  gh.lazySingleton<_i4.FlutterSecureStorage>(() => appModule.secureStorage);
+  gh.lazySingleton<_i5.LocalStorage>(
+      () => _i5.LocalStorageImpl(get<_i4.FlutterSecureStorage>()));
   gh.lazySingleton<_i6.RemoteConfigDataSource>(
       () => _i6.RemoteConfigDataSourceImpl(get<_i3.FirebaseRemoteConfig>()));
   gh.lazySingleton<_i7.LocalConfigDataSource>(
-      () => _i7.LocalConfigDataSourceImpl(get<_i4.LocalStorage>()));
+      () => _i7.LocalConfigDataSourceImpl(get<_i5.LocalStorage>()));
   gh.lazySingleton<_i8.RemoteConfigRepository>(() =>
       _i9.RemoteConfigRepositoryImpl(
           get<_i6.RemoteConfigDataSource>(), get<_i7.LocalConfigDataSource>()));
@@ -44,9 +51,18 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
       () => _i10.SaveRemoteConfigApiKey(get<_i8.RemoteConfigRepository>()));
   gh.factory<_i11.SaveRemoteConfigApiUrl>(
       () => _i11.SaveRemoteConfigApiUrl(get<_i8.RemoteConfigRepository>()));
-  gh.factory<_i12.FetchRemoteConfigApiUrlUseCase>(() =>
-      _i12.FetchRemoteConfigApiUrlUseCase(get<_i8.RemoteConfigRepository>()));
+  gh.factory<_i12.FetchRemoteConfigApiKeyUseCase>(() =>
+      _i12.FetchRemoteConfigApiKeyUseCase(get<_i8.RemoteConfigRepository>()));
+  gh.factory<_i13.FetchRemoteConfigApiUrlUseCase>(() =>
+      _i13.FetchRemoteConfigApiUrlUseCase(get<_i8.RemoteConfigRepository>()));
+  gh.factory<_i14.SplashCubit>(() => _i14.SplashCubit(
+      get<_i12.FetchRemoteConfigApiKeyUseCase>(),
+      get<_i13.FetchRemoteConfigApiUrlUseCase>(),
+      get<_i10.SaveRemoteConfigApiKey>(),
+      get<_i11.SaveRemoteConfigApiUrl>()));
   return get;
 }
 
-class _$FirebaseModule extends _i13.FirebaseModule {}
+class _$FirebaseModule extends _i15.FirebaseModule {}
+
+class _$AppModule extends _i16.AppModule {}
