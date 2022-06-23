743da6cb87054aa908c85fdf301381e00eaa6439
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-06-23 23:46:31 +0700

    remove unused import

--- lib/core/di/dio_module.dart
+++ lib/core/di/dio_module.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:weather/core/network/adapters/dio_adapter_stubs.dart'
