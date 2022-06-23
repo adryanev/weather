d16a78d1ff5122c63c89f98bfae83eb8bb96420f
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-06-24 00:20:24 +0700

    change test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -5,12 +5,20 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weather/app/app.dart';
+import 'package:weather/core/utils/constants.dart';
+import 'package:weather/injector.dart';
+
+import '../../helpers/firebase/mock.dart';
 
 void main() {
+  setupFirebaseRemoteConfigMocks();
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders Pages', (tester) async {
+      await Firebase.initializeApp();
+      await configureDependencies(environment: Environment.envDev);
       await tester.pumpWidget(const App());
       expect(true, true);
     });
