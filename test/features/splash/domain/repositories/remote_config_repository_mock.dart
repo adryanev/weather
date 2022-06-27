68cf7f09d0cf4f1e86896c7fcf63f8fada5fb066
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-06-27 20:48:04 +0700

    delete unit test

--- test/features/splash/domain/repositories/remote_config_repository_mock.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:weather/features/splash/domain/repositories/remote_config_repository.dart';
-
-class RemoteConfigRepositoryMock extends Mock
-    implements RemoteConfigRepository {}
